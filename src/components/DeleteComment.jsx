3518eccea2d782fe5e8985e95591ec6924881e94
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-06-08 16:56:30 +0100

    Update DeleteComment.jsx

--- src/components/DeleteComment.jsx
+++ src/components/DeleteComment.jsx
@@ -1,15 +1,14 @@
-import { Component } from 'react'
+// import { Component } from 'react'
 import { Button } from 'react-bootstrap'
 
-class DeleteComment extends Component {
-    state = {  }
+const DeleteComment = (props) => {
 
-    deleteComment = async () => {
+    const deleteComment = async () => {
         const apiToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGFlM2Y4MWNlYWY0ODAwMTVjOTE4NmEiLCJpYXQiOjE2MjIwMzIyNTcsImV4cCI6MTYyMzI0MTg1N30.COuaWwE7g5o-UfUez4tVCPw0zZc5llB7Jqgsp37LrSA'
         const apiUrl = 'https://striveschool-api.herokuapp.com/api/comments/'
-        // console.log(this.props.comment._id)
+        // console.log(props.comment._id)
         try {
-            const response = await fetch(apiUrl + this.props.comment._id, {
+            const response = await fetch(apiUrl + props.comment._id, {
                 method: 'DELETE',
                 headers: {
                     "Authorization": `Bearer ${apiToken}`
@@ -17,7 +16,7 @@ class DeleteComment extends Component {
             })
             // console.log(response.ok)
             if (response.ok ){
-                this.props.onDeleteComment(this.props.comment._id)
+                props.onDeleteComment(props.comment._id)
                 alert('Comment deleted successfully')
             } else {
                 alert('There was an error, please try againg')
@@ -27,15 +26,13 @@ class DeleteComment extends Component {
         }
     }
 
-    render() { 
         return (  
             <>
-                <Button size='sm' variant="danger" onClick={(e) => this.deleteComment()}>
+                <Button size='sm' variant="danger" onClick={(e) => deleteComment()}>
                     <small>Delete</small>
                 </Button>
             </>
         );
-    }
 }
  
 export default DeleteComment;
