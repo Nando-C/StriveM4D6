2ebf0ec8cdd95ead9cf0e665c63c0c248310f295
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-06-08 17:13:46 +0100

    Update EditComment.jsx

--- src/components/EditComment.jsx
+++ src/components/EditComment.jsx
@@ -1,44 +1,39 @@
-import { Component } from 'react'
+// import { Component } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { useState } from 'react'
 
-class EditComment extends Component {
-    state = {  
-        review: {
-            comment: '',
-            rate: 'Select Rating',
-            // elementId: this.props.comment._id
-        } 
-    }
+const EditComment = (props) => {
+    const [review, setReview] = useState({
+        comment: '',
+        rate: 'Select Rating',
+    })
 
-    inputChange = (e) => {
-        // console.log(e.target.value)
-        this.setState({
-            review: {
-                ...this.state.review,
+    const inputChange = (e) => {
+        setReview(
+            {
+                ...review,
                 [e.target.id]: e.target.value,
             }
-        })
+        )
     }
 
-    submitUpdate = async (e) => {
+    const submitUpdate = async (e) => {
         e.preventDefault() 
 
         const apiToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGFlM2Y4MWNlYWY0ODAwMTVjOTE4NmEiLCJpYXQiOjE2MjIwMzIyNTcsImV4cCI6MTYyMzI0MTg1N30.COuaWwE7g5o-UfUez4tVCPw0zZc5llB7Jqgsp37LrSA'
         const apiUrl = 'https://striveschool-api.herokuapp.com/api/comments/'
-        // console.log(this.props.comment._id)
         try {
-            const response = await fetch(apiUrl + this.props.comment._id, {
+            const response = await fetch(apiUrl + props.comment._id, {
                 method: 'PUT',
-                body: JSON.stringify(this.state.review),
+                body: JSON.stringify(review),
                 headers: {
                     "Authorization": `Bearer ${apiToken}`,
                     "Content-type": "application/json"
                 }
             })
-            // console.log(response.ok)
             if (response.ok ){
-                this.props.setEdit()
-                this.props.updateComment(await response.json())
+                props.setEdit()
+                props.updateComment(await response.json())
                 alert('Comment updated successfully')
             } else {
                 alert('There was an error, please try againg')
@@ -49,15 +44,14 @@ class EditComment extends Component {
     }
 
     
-    render() { 
         return (  
             <>
-            <Form className="my-3" onSubmit={(e) => this.submitUpdate(e)} >
+            <Form className="my-3" onSubmit={(e) => submitUpdate(e)} >
                         <Form.Group className='px-3'>
-                            <Form.Control id="comment" as="textarea" rows={3} placeholder="Write a review" value={this.state.review.comment} onChange={e => this.inputChange(e)}/>
+                            <Form.Control id="comment" as="textarea" rows={3} placeholder="Write a review" value={review.comment} onChange={e => inputChange(e)}/>
                         </Form.Group>
                         <Form.Group className='px-3'>
-                            <Form.Control id="rate" size='sm' as="select" value={this.state.review.rate}  onChange={e => this.inputChange(e)}>
+                            <Form.Control id="rate" size='sm' as="select" value={review.rate}  onChange={e => inputChange(e)}>
                                 <option>Select Rating</option>
                                 <option>1</option>
                                 <option>2</option>
@@ -69,11 +63,9 @@ class EditComment extends Component {
                         <Button size='sm' variant="primary" type="submit">
                             <small>Update</small>
                         </Button>
-                        {/* <EditComment comment={this.props.comment}/> */}
                     </Form>
             </>
         );
-    }
 }
  
 export default EditComment;
