bacb638134308856e66029f4d3fc81469c2a76c9
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-06-08 17:29:10 +0100

    Update CommentsList.jsx

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,19 +1,15 @@
-import { Component } from 'react'
+// import { Component } from 'react'
 import { ListGroup } from 'react-bootstrap'
 import CommentItem from './CommentItem'
 
-class CommentsList extends Component {
-    state = {  
+const CommentsList = (props) => {
 
-    }
-    render() { 
         return ( 
             <ListGroup className='py-3'>
                 <h5 className='pl-3'>Reviews</h5>
-                {this.props.comments && this.props.comments.map(comment => <CommentItem key={comment._id} comment={comment} onDeleteComment={this.props.onDeleteComment} updateComment={this.props.updateComment}/>)}
+                {props.comments && props.comments.map(comment => <CommentItem key={comment._id} comment={comment} onDeleteComment={props.onDeleteComment} updateComment={props.updateComment}/>)}
             </ListGroup>
         );
-    }
 }
  
 export default CommentsList
